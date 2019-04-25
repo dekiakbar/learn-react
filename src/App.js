8c3ef17f65e12db9f4d01e18818278c741038e27
Deki <dekiakbar@linuxmail.org>
2019-04-25 22:54:24 +0700

    create-react-app commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/scss/bootstrap.scss';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <button className="btn btn-sm btn-primary">test</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
